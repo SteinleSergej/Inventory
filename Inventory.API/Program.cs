7d7df692b8e9610980aebfb41dee582b1a0112c2
SteinleSergej <steinle.sergej.prog@gmail.com>
2024-06-08 14:35:15 +0200

    Add project files.

---
+++ Inventory.API/Program.cs
@@ -0,0 +1,21 @@
+using Inventory.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Inventory.Domain/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty ;
+
+        public double Price { get; set; }
+
+        public int QuantityInStock { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
